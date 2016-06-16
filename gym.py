eb3676f40f9fca5c9158aa91187923703988f0fa
David Yell <dyell@ukwebmedia.com>
2016-06-16 09:36:27 +0100

    Calculated cost per minute

--- gym.py
+++ gym.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+#!/usr/bin/env python3
 
 from decimal import *
 
@@ -29,14 +30,26 @@ class Gym:
     def total_saved_per_month(self):
         return Decimal(self.payPerVisit) * (Decimal(self.visitsPerWeek) * 4) - Decimal(self.pricePerMonth)
 
+    def cost_per_minute(self, pay_type):
+        if pay_type == 'per_visit':
+            return (Decimal(self.payPerVisit) * 100) / Decimal(self.minutesPerVisit)
+        else:
+            return (self.subscription_cost_per_visit() * 100) / Decimal(self.minutesPerVisit)
+
 gym = Gym()
 gym.pricePerMonth = input("How much is the monthly subscription fee? ")
 gym.visitsPerWeek = input("How many gym visits in a week? ")
 gym.payPerVisit = input("How much does it cost to use the gym on pay per use? ")
 gym.minutesPerVisit = input("How many minutes do you spend at the gym each session? ")
 
+print('\n')
 print('Subscription cost per visit: £', gym.subscription_cost_per_visit())
-print('Subscription monthly cost: £', gym.subscription_total_cost())
 print('Pay per use: £', gym.pay_per_use_cost_per_visit())
+
+print('Subscription monthly cost: £', gym.subscription_total_cost())
 print('Pay per use monthly cost: £', gym.pay_per_use_total_cost())
+
 print('Saving each month: £', gym.total_saved_per_month())
+
+print('Cost in pence per minute for subscription: ', gym.cost_per_minute('subscription'))
+print('Cost in pence per minute for pay per use: ', gym.cost_per_minute('per_visit'))
